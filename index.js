97c94a589abdb842d2718807cc0b0790f84e0bc4
SamanjaCartagena <cart.samanja09@gmail.com>
2022-03-15 16:50:14 -0400

    executables added

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const port = 3000;
 
 
 const server= http.createServer((req, res) =>{
+   // console.log(req);
+    const {url} = req;
+    console.log(url);
+   
+    /**if(url === '/translations'){
+        const translations ={1:'one',2:'two',3:'three' }
+        
+        res.setHeader('Content-Type','application/json');
+
+        res.write(JSON.stringify(translations));
+        res.end();
+    }
+    **/
+
    res.end('Welcome to node');
 });
 
